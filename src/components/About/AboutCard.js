7152e4521686748602bb78b86dfd14f00259cd7f
adityasimant <adityasimant88@gmail.com>
2023-01-12 19:31:47 +0530

    initial

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -17,7 +17,7 @@ function AboutCard() {
           </p>
           <ul>
             <li className="about-activity">
-              <ImPointRight /> Playing Games
+              <ImPointRight /> Playing Chess
             </li>
             <li className="about-activity">
               <ImPointRight /> Writting Tech Blogs
@@ -27,10 +27,6 @@ function AboutCard() {
             </li>
           </ul>
 
-          <p style={{ color: "rgb(155 126 172)" }}>
-            "Strive to build things that make a difference!"{" "}
-          </p>
-          <footer className="blockquote-footer">Soumyajit</footer>
         </blockquote>
       </Card.Body>
     </Card>
