3c052cdd0d2d10dde8d635411d4c459ff48654c9
adityasimant <adityasimant88@gmail.com>
2023-01-12 20:08:20 +0530

    resume changes

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,14 +2,14 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/Soumyajit_Behera-BIT_MESRA.pdf";
+import pdf from "../../Assets/aadisimtresume.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://raw.githubusercontent.com/soumyajit4419/portfolio/master/src/Assets/Soumyajit_Behera-BIT_MESRA.pdf";
+  "https://firebasestorage.googleapis.com/v0/b/local-3722d.appspot.com/o/aadisimtresume.pdf.pdf?alt=media&token=ccbf9341-36de-44e8-962a-15848576ae37";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
